873f764da824cf64f969b2694880a9ce3de4ad82
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2022-01-16 00:25:41 +0900

    [Add] package, retrofit & room & hilt dependency

--- app/src/main/java/com/nexters/checkareer/view/MainActivity.kt
+++ app/src/main/java/com/nexters/checkareer/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nexters.checkareer
+package com.nexters.checkareer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nexters.checkareer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
